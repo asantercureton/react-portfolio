eb342dd5e96d238d23ad78e06769af511f089254
Asante Cureton <asantercureton@gmail.com>
2021-11-01 01:15:12 -0400

    Finished styling page

--- react-portfolio/src/components/AboutMe/index.js
+++ react-portfolio/src/components/AboutMe/index.js
@@ -5,8 +5,8 @@ export default class AboutMe extends Component {
             <section id="about">
                 <div className="row">
                     <div className="nine columns main-col">
-                        <img src={`${process.env.PUBLIC_URL}/assets/images/IMG_20210627_170627_803.jpg`} alt="/" width="300" height="300" class="aboutMe" />
-                        <section id="aboutMe">
+                        <img id="aboutMe" src={`${process.env.PUBLIC_URL}/assets/images/IMG_20210627_170627_803.jpg`} alt="/" width="300" height="300" class="aboutMe" />
+                        <section >
                             <article>
                                 <h2 className="aboutH2">About Me</h2>
                                 <p className="aboutP">I am an Engineering Grad of NC State University, where I also played D-1 Football for the Wolfpack. I have experience in web design (REACT, CSS and Front-End Javascript), along with skillsets acquired via working with Controls and PLCs. In my free time, I love painting and writing family books that provide positive energy and spiritual growth.</p>
--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -3,38 +3,38 @@ export default class Porfolio extends Component {
     render() {
         return (
             <section id="portfolio">
-                <div class="experience">
-                    <section id="work" class="work">
+                <div className="experience">
+                    <section id="work" className="work">
                         <h2>Professional Portfolio</h2>
                         <article className="article">
-                            <div>
-                                <div>
-                                    <a href="https://little-village-10292021.herokuapp.com/">
-                                        <img src={`${process.env.PUBLIC_URL}/assets/images/little-village.jpg`} alt="/" width="125%" height="125%" />
-                                    </a>
+                            <div className="proCard">
+                                <a href="https://little-village-10292021.herokuapp.com/">
+                                    <img className="image" src={`${process.env.PUBLIC_URL}/assets/images/little-village.jpg`} alt="/" width="300" height="auto" />
+                                </a>
 
-                                    <div className="cardTitle">LITTLE VILLAGE APP (MERN)</div>
-                                </div>
+                                <div className="cardTitle">LITTLE VILLAGE APP (MERN Stack)</div>
+
+                                <a href="https://ghostly-stories-10-4-2021.herokuapp.com/">
+                                    <img className="image" src={`${process.env.PUBLIC_URL}/assets/images/gs.png`} alt="/" width="300" height="auto" />
+                                </a>
+                                <div className="cardTitle">GHOSTLY STORIES APP (Express/Node.JS)</div>
+
+
+
+                                <a href="https://github.com/asantercureton">
+                                    <img className="image" src={`${process.env.PUBLIC_URL}/assets/images/matrix.jpg`} alt="/" width="300" height="auto" />
+                                </a>
+
+                                <div className="cardTitle">MORE PROJECTS!</div>
+
                             </div>
 
                             <div>
-                                <div>
-                                    <a href="https://ghostly-stories-10-4-2021.herokuapp.com/">
-                                        <img src={`${process.env.PUBLIC_URL}/assets/images/gs.png`} alt="/" width="125%" height="125%" />
-                                    </a>
-                                    <div className="cardTitle">GHOSTLY STORIES APP (Express)</div>
 
-                                </div>
                             </div>
 
                             <div>
-                                <div>
-                                    <a href="https://github.com/asantercureton">
-                                        <img src={`${process.env.PUBLIC_URL}/assets/images/gs.png`} alt="/" width="125%" height="125%" />
-                                    </a>
 
-                                    <div className="cardTitle">MORE PROJECTS!</div>
-                                </div>
                             </div>
                         </article>
                     </section>
